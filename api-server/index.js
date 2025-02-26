4b3598cd42e3bf540780bb4b2dc2c90458f41809
anojaagnanes <anojaa16@yahoo.com>
2025-02-26 21:41:29 +0530

    first commit

--- api-server/index.js
+++ api-server/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
-const {getMinNumber} = require('./util.js');
+const {getMinNumber,getMaxNumber,getAvgOfTwoNumber,getSortedNumber} = require('./util.js');
 
 const app = new express();
 const port = 3000;
+//to find max number
+app.get('/number/max', (req, res) => {
+  const num1 = parseFloat(req.query.num1);//query parameter
+  const num2 = parseFloat(req.query.num2);
 
-app.get('/number/max', (req, res) => {});
-app.get('/number/avg', (req, res) => {}); ///number/avg?numbers=1,4,7,44,676,......n
-app.get('/number/sort', (req, res) => {}); ///number/sort?numbers=1,4,7,44,676,......n&type (asc |dec)
+  const result = getMaxNumber(num1, num2);
+
+  res.status(result.status).json(result.data);
+});
+//to find average 
+app.get('/number/avg', (req, res) => {
+  const num1 = parseFloat(req.query.num1);//query parameter
+  const num2 = parseFloat(req.query.num2);
+
+  const result = getAvgOfTwoNumber(num1, num2);
+
+  res.status(result.status).json(result.data);
+}); ///number/avg?numbers=1,4,7,44,676,......n
+
+//to find sort
+
+app.get('/number/sort', (req, res) => {
+  const numbersParam = req.query.numbers;
+  const type = req.query.type;
+  //console.log("============");
+	//console.log(type);
+	//console.log("============");
+  const result = getSortedNumber(numbersParam,type);
+  
+
+  res.status(result.status).json(result.data);
+}); ///number/sort?numbers=1,4,7,44,676,......n&type (asc |dec)
 app.get('/number/count', (req, res) => {}); ///number/count?numbers=1,A,saman,Kamal,676,......n&search=saman //need to return how many occurances
 
+//to find min number 
 app.get('/number/min', (req, res) => {
-  const num1 = parseFloat(req.query.num1);
+  const num1 = parseFloat(req.query.num1);//query parameter
   const num2 = parseFloat(req.query.num2);
 
   const result = getMinNumber(num1, num2);
--- api-server/util.js
+++ api-server/util.js
@@ -1,5 +1,5 @@
 function getMinNumber(num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
+  if (isNaN(num1) || isNaN(num2)) { //isNaN method inbuild for check num or not
     return {
       status: 400,
       data: {
@@ -9,8 +9,110 @@ function getMinNumber(num1, num2) {
   }
   return {
     status: 200,
-    data: {min: Math.min(num1, num2)},
+    data: {min: Math.min(num1, num2)}, //inbuild method 
   };
 }
 
-module.exports = {getMinNumber};
+function getMaxNumber(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) { //isNaN method inbuild for check num or not
+    return {
+      status: 400,
+      data: {
+        error: `both parameters should be numbers then only print `,
+      },
+    };
+  }
+  return {
+    status: 200,
+    data: {max: Math.max(num1, num2)}, //inbuild method 
+  };
+}
+
+function getAvgOfTwoNumber(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) { //isNaN method inbuild for check num or not
+    return {
+      status: 400,
+      data: {
+        error: `both parameters should be numbers then only print `,
+      },
+    };
+  }
+  let avg = (num1 + num2)/2;
+  return {
+    status: 200,
+    data: {average: avg}
+  };
+}
+
+function getSortedNumber(numArray,type) {
+  const convertArray = numArray.split(',').map(num => parseInt(num)); //spliting with , 
+  console.log(convertArray);
+  convertArray.forEach((item) => {
+    if (isNaN(item)) {  //num or not num
+      return {
+        status: 400,
+        data: {
+        error: `All parameters should be numbers`,
+        },
+      };
+	}
+  });
+  let sortlistArr;
+  let type2 = type.replace(/['"]/g, "");
+	if(type2 =="asc"){
+    console.log("anoj11122a");
+		sortlistArr = assending(convertArray);
+	}
+  else
+  {
+    sortlistArr = sortDescending(convertArray);
+  }
+  //convertArray.sort()
+ // console.log("Sorted: " + convertArray);
+ const newStr = type.replace(/['"]/g, "");
+	console.log("=====uuuuuuuuuuu=======");
+	console.log(newStr);
+	console.log("=====uuuuuuuuuuu=======");
+  console.log(type);
+  return {
+      status: 200,
+      data: {sorted:sortlistArr}
+    };
+}
+
+function assending (arr) {
+  console.log("anojaa");
+  let n = arr.length;
+  
+  for (let i = 0; i < n; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+}
+
+function sortDescending(array) {
+  for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - i - 1; j++) {
+          if (array[j] < array[j + 1]) {
+              // Swap elements if they are in the wrong order
+              let temp = array[j];
+              array[j] = array[j + 1];
+              array[j + 1] = temp;
+          }
+      }
+  }
+    return array;
+  }
+
+module.exports = {getMinNumber,getMaxNumber,getAvgOfTwoNumber,getSortedNumber};
